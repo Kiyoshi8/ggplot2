03e8138aaacce00da17f658383c9724f57eab477
Kiyoshi C. Rio <86674319+Kiyoshi8@users.noreply.github.com>
2023-09-03 13:25:52 +0800

    Add files via upload

---
+++ ggplot2.R
@@ -0,0 +1,19 @@
+#Case Sensitivity
+
+myobject = 4:8
+
+myObject # upper case O gives an error
+
+myobject # all lower cases
+
+#Concatenation with c
+
+a = 6:10
+
+b = c(6:10)
+
+?plot.ts
+
+plot.ts(c(6,3,9,8,3)) # correct form, 1st argument is provided
+
+plot.ts(6,3,9,8,3) # incorrect, 5 arguments are provided
